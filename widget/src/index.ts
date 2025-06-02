e8712a0e377bc07d475207eb589b5189306e235c
punkystone <email@punkystone.de>
2025-06-02 10:02:06 +0200

    add websocket to widget

--- widget/src/index.ts
+++ widget/src/index.ts
@@ -1,6 +1,5 @@
 window.addEventListener("onWidgetLoad", function (data) {
     const format = data.detail.fieldData["format"];
-    if (format === undefined) return;
     const months = [
         "Tjanuar",
         "Tjebruar",
@@ -16,7 +15,12 @@ window.addEventListener("onWidgetLoad", function (data) {
         "Tjecember",
     ];
     const monthElement = document.querySelector(".month");
+    const donationElement = document.querySelector(".donation");
+    if (donationElement === null) return;
     if (monthElement === null) return;
+    if (format === undefined) return;
+    const donationUrl = "wss://tjan.punkystone.de/donation";
+    startSocket(donationUrl, donationElement);
     const updateLoop = (): void => {
         const now = new Date();
         const monthName = months[now.getMonth()];
@@ -26,3 +30,17 @@ window.addEventListener("onWidgetLoad", function (data) {
     updateLoop();
     setInterval(updateLoop, 1000);
 });
+
+const startSocket = (donationUrl: string, donationElement: Element): void => {
+    const wss = new WebSocket(donationUrl);
+    wss.addEventListener("message", (event: MessageEvent): void => {
+        const data = event.data as string;
+        donationElement.innerHTML = data;
+    });
+    wss.addEventListener("close", (): void => {
+        console.log("Connection closed, retrying in 5 seconds");
+        setTimeout(function () {
+            startSocket(donationUrl, donationElement);
+        }, 5000);
+    });
+};
